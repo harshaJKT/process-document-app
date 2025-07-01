597aeb22d2311376955e74e0f731f8fbd96dff6c
white Rabbit <deoraj.gope@jktech.com>
2025-07-01 01:31:50 +0000

    complete upload_document functionality and add message key original_name

--- app/routers/upload.py
+++ app/routers/upload.py
@@ -7,6 +7,10 @@ import config
 
 router = APIRouter()
 
+def generate_unique_filename(extension="txt"):
+    unique_name = f"{uuid.uuid4()}.{extension}"
+    return unique_name
+
 
 @router.post("/upload", status_code=201)
 async def upload_document(
@@ -22,16 +26,33 @@ async def upload_document(
     try:
         # TODO: Save file locally
 
+        upload_directory = "uploads"
+
+        if not os.path.exists(upload_directory):
+            os.makedirs(upload_directory)
+
+        
+        ext = os.path.splitext(file.filename)[1]
+        unique_filename = f"{uuid.uuid4()}{ext}"
+        destination_file_path = os.path.join(upload_directory, unique_filename)
+        with open(destination_file_path, "wb") as buffer:
+            contents = await file.read()  # read the uploaded file content
+            buffer.write(contents) 
+
         # TODO: Publish message to topic for processing
         # Messaging is already implemented in the messaging.py file, refer to it and use it.
         # Think of all the keys that should be present in the message while publishing the message to the topic.
         # Use: await broker.publish("doc_uploaded", message)
 
-        return {
+        message =  {
             "message": "File uploaded and queued for processing",
-            "file_path": "",  # TODO: file_path
+            "file_path": destination_file_path,  # TODO: file_path
             "role": role,
+            "original_name":unique_filename
         }
+
+        await broker.publish("doc_uploaded",message )
+        return message
 
     except Exception as e:
         return {"error": str(e)}
