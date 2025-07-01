ac793975a4df2406b810e3673e3e52e5bb4cf004
white Rabbit <deoraj.gope@jktech.com>
2025-07-01 01:32:53 +0000

    create user functionalities to store users and their roles in db

--- app/routers/user_role.py
+++ app/routers/user_role.py
@@ -1,11 +1,35 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.database import get_db
+import psycopg2
 from app.models import UserRoleMap
 import uuid
 from pydantic import BaseModel
+from fastapi import HTTPException
+import config
 
 router = APIRouter()
+
+def query_db_postgres(dburl:str , table , columns:list):
+    try:
+        conn = psycopg2.connect(dburl)
+        cursor = conn.cursor()
+
+        cols_str = ', '.join(columns)
+        query = f"select {cols_str} from {table};"
+
+        cursor.execute(query)
+
+        rows = cursor.fetchall()
+        print(rows)
+        return rows
+    except Exception as e:
+        print("Error querying db for user roles ")
+        return []
+    finally:
+        cursor.close()
+        conn.close()
+
 
 
 class UserRoleCreate(BaseModel):
@@ -38,11 +62,13 @@ def create_user_role(user_role: UserRoleCreate, db: Session = Depends(get_db)):
         db.refresh(new_user_role)
 
         return UserRoleResponse(
+            id=str(new_user_role.id),
             user=user_role.user,
             role=user_role.role,
         )
     except Exception as e:
-        return {"error": str(e)}
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 
 @router.get("/user-role", response_model=list[UserRoleResponse])
@@ -54,10 +80,19 @@ def get_all_user_roles(db: Session = Depends(get_db)):
     """
     try:
         # TODO: Query all UserRoleMap records
+        dburl = config.DATABASE_URL
+        table = "user_role_map"
+        cols = ['id' , '"user"' , 'role']
+        user_roles = query_db_postgres(dburl , table , cols)
 
         # TODO: Convert to response format
+        #normalizing the col name as user is a keyword in postgres
+        normalized_cols = [col.strip('"') for col in cols]
 
-        return []  # Placeholder
+
+        result = [dict(zip(normalized_cols,row)) for row in user_roles]
+
+        return   result# Placeholder
     except Exception as e:
         return {"error": str(e)}
 
